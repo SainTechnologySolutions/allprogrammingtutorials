664cd0b5249e20adf74667143c27bcc90c89b124
Amit Kumar <amit.kumar@saintech.co.in>
2018-10-21 19:17:33 +0530

    Added method level documentation to python factory classes

--- python-demos/designpatterns/factory.py
+++ python-demos/designpatterns/factory.py
@@ -3,12 +3,16 @@ from tservices import *
 
 class TutorialServiceFactory:
     def __init__(self):
+        # Get all subclasses of TutorialService
         subclasses = TutorialService.__subclasses__()
+        # Initialize classes object where subclasses will be stored along with type
         self.classes = {}
         for subclass in subclasses:
+            # Add type as key and subclass as value in classes object
             self.classes[subclass.get_type()] = subclass
 
     def get_tutorial_service(self, type):
+        # Get class object corresponding to input type
         return self.classes[type]
 
 
--- python-demos/designpatterns/tservices/tutorial_service.py
+++ python-demos/designpatterns/tservices/tutorial_service.py
@@ -7,12 +7,21 @@ class TutorialService(object):
 
     @staticmethod
     def get_type():
+        """
+        Defines unique type of this service.
+        """
         pass
 
     @abstractmethod
     def get_tutorial_by_id(self, tutorial_id):
+        """
+        Gets the tutorial for input tutorial id.
+        """
         pass
 
     @abstractmethod
     def save_tutorial(self, tutorial):
+        """
+        Saves the tutorial in underlying store.
+        """
         pass
